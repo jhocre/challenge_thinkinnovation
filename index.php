09ef87cf8f270ae0fe57cbb237c4400194fd2df6
johann <johann.creneguy@gmail.com>
2015-12-10 16:10:58 +0100

    HTML architecture only + mockup

---
+++ index.php
@@ -0,0 +1,365 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Challenge - thinkinnovation</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous"><link href="css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- Font awesome -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+	
+	<!-- My own style sheet -->
+	<link rel="stylesheet" href="style.css">
+	
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div id="main">
+    	<header class="container">	
+    		<div class="row">
+    			
+    				<img class="col-sm-3 col-xs-12" alt="test_logo" src="http://fakeimg.pl/350x200/?text=World&font=lobster">
+    			
+    		</div>
+    	</header>
+    	
+    	<section class="container">
+    		<div class="row">
+	    		<div class="col-md-6 col-xs-12">
+	    			<div class="row">
+	    				<div class="col-md-12">
+	    					<h2></h2>
+	    				</div>
+	    				<div class="col-md-12">
+	    					<video src=""></video>
+	    				</div>
+	    			</div>
+				</div>
+				<div class="col-md-6 col-xs-12">
+					<h2></h2>
+					<p></p>
+					<p></p>
+				</div>
+				<div class="col-md-12"></div>
+			</div>
+    	</section>
+    		
+    	<section class="container">
+    		<div class="row">
+    			<div class="col-md-12">
+    				<h2></h2>
+    			</div>
+    			<div class="col-md-12">
+    				<h3></h3>
+    				<p></p>
+    			</div>
+    			
+    			<article id="active" class="col-md-12">
+    					<video src=""></video>
+    			</article>	
+    			
+    			<article class="col-md-4 col-sm-6 col-xs-12">
+    				<div class="row">
+    					<div class="col-md-12">
+    						<video src=""></video>
+    					</div>
+    					<div class="col-md-12">
+    						<h4></h4>
+    						<h5></h5>
+    					</div>
+    				</div>
+    			</article>
+    			
+    			<article class="col-md-4 col-sm-6 col-xs-12">
+    				<div class="row">
+    					<div class="col-md-12">
+    						<video src=""></video>
+    					</div>
+    					<div class="col-md-12">
+    						<h4></h4>
+    						<h5></h5>
+    					</div>
+    				</div>
+    			</article>
+    			
+    			<article class="col-md-4 col-sm-6 col-xs-12">
+    				<div class="row">
+    					<div class="col-md-12">
+    						<video src=""></video>
+    					</div>
+    					<div class="col-md-12">
+    						<h4></h4>
+    						<h5></h5>
+    					</div>
+    				</div>
+    			</article>
+    			
+    			<article class="col-md-4 col-sm-6 col-xs-12">
+    				<div class="row">
+    					<div class="col-md-12">
+    						<video src=""></video>
+    					</div>
+    					<div class="col-md-12">
+    						<h4></h4>
+    						<h5></h5>
+    					</div>
+    				</div>
+    			</article>
+    			
+    			<article class="col-md-4 col-sm-6 col-xs-12">
+    				<div class="row">
+    					<div class="col-md-12">
+    						<video src=""></video>
+    					</div>
+    					<div class="col-md-12">
+    						<h4></h4>
+    						<h5></h5>
+    					</div>
+    				</div>
+    			</article>
+    			
+    			<article class="col-md-4 col-sm-6 col-xs-12">
+    				<div class="row">
+    					<div class="col-md-12">
+    						<video src=""></video>
+    					</div>
+    					<div class="col-md-12">
+    						<h4></h4>
+    						<h5></h5>
+    					</div>
+    				</div>
+    			</article>
+    			
+    			<!--  <div class="col-md-12">
+    				<p></p>
+    			</div>	-->
+    		</div>
+    	</section>
+    	
+    	<section class="container">
+    		<div class="row">
+    			<div class="col-md-12">
+    				<h2></h2>
+    			</div>
+    			<div class="col-md-12">
+    				<h3></h3>
+    				<p></p>
+    			</div>
+    		</div>
+    		<div class="row">
+    		
+    		
+    			<article class="col-md-8 col-sm-12 ">
+    				<div class="row">
+    					<div class="col-md-12">
+    						<video  width="100%"  src="http://www.sample-videos.com/video/flv/720/big_buck_bunny_720p_1mb.flv" controls></video>
+    					</div>
+    					<div class="col-md-12">
+    						<h4></h4>
+    						<h5></h5>
+    					</div>
+    				</div>
+    			</article>
+    			
+    			<div class="col-md-4 col-sm-12">
+    				<div class="row">
+		    			<article class="col-md-12 col-sm-6 col-xs-12">
+		    				<div class="row">
+		    					<div class="col-md-12">
+		    						<video src=""></video>
+		    					</div>
+		    					<div class="col-md-12">
+		    						<h4></h4>
+		    						<h5></h5>
+		    					</div>
+		    				</div>
+		    			</article>
+		    			
+		    			<article class="col-md-12 col-sm-6 col-xs-12">
+		    				<div class="row">
+		    					<div class="col-md-12">
+		    						<video src=""></video>
+		    					</div>
+		    					<div class="col-md-12">
+		    						<h4></h4>
+		    						<h5></h5>
+		    					</div>
+		    				</div>
+		    			</article>
+		    			
+		    		</div>	
+	    		</div>
+	    			
+    		</div>
+    	</section>
+    	
+    	<section class="container">
+    		<div class="row">
+    			<div class="col-md-12">
+    				<p></p>
+    				<p></p>
+    			</div>
+    		
+	    		<div class="col-md-12">
+	    			<h2></h2>
+	    		</div>
+	    		<div class="col-md-12">
+	    			<h3></h3>
+	    		</div>
+    		</div>
+    		
+    		<div class="row">
+	    		<form action="">
+	    			<div class="col-md-4 col-sm-6 col-xs-12">
+		    			<div class="radio">
+						  	<label>
+						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+						    	answer 1
+						  	</label>
+						</div>
+						<div class="radio">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								answer 2  
+							</label>
+						</div>
+						<div class="radio disabled">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+						    	answer 3
+						  	</label>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-sm-6 col-xs-12">
+		    			<div class="radio">
+						  	<label>
+						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+						    	answer 1
+						  	</label>
+						</div>
+						<div class="radio">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								answer 2  
+							</label>
+						</div>
+						<div class="radio disabled">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+						    	answer 3
+						  	</label>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-sm-6 col-xs-12">
+		    			<div class="radio">
+						  	<label>
+						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+						    	answer 1
+						  	</label>
+						</div>
+						<div class="radio">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								answer 2  
+							</label>
+						</div>
+						<div class="radio disabled">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+						    	answer 3
+						  	</label>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-sm-6 col-xs-12">
+		    			<div class="radio">
+						  	<label>
+						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+						    	answer 1
+						  	</label>
+						</div>
+						<div class="radio">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								answer 2  
+							</label>
+						</div>
+						<div class="radio disabled">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+						    	answer 3
+						  	</label>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-sm-6 col-xs-12">
+		    			<div class="radio">
+						  	<label>
+						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+						    	answer 1
+						  	</label>
+						</div>
+						<div class="radio">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								answer 2  
+							</label>
+						</div>
+						<div class="radio disabled">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+						    	answer 3
+						  	</label>
+						</div>
+					</div>
+					
+					<div class="col-md-4 col-sm-6 col-xs-12">
+		    			<div class="radio">
+						  	<label>
+						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
+						    	answer 1
+						  	</label>
+						</div>
+						<div class="radio">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios2" value="option2">
+								answer 2  
+							</label>
+						</div>
+						<div class="radio disabled">
+						  	<label>
+						    	<input type="radio" name="optionsRadios" id="optionsRadios3" value="option3">
+						    	answer 3
+						  	</label>
+						</div>
+					</div>
+					
+					
+	    		</form>
+    		</div>
+    	</section>
+    </div> 
+    
+    
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+  </body>
+</html>
