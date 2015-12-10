9d017bfe01cf51cc1c7411f90a38145f8df5fb7c
johann <johann.creneguy@gmail.com>
2015-12-10 17:44:41 +0100

    I add : content to the HTML tags, generic videos, and background colors

--- index.php
+++ index.php
@@ -29,57 +29,81 @@
   </head>
   <body>
     <div id="main">
+    
+    <!-- HEADER + logo -->
+    
     	<header class="container">	
     		<div class="row">
-    			
     				<img class="col-sm-3 col-xs-12" alt="test_logo" src="http://fakeimg.pl/350x200/?text=World&font=lobster">
-    			
     		</div>
     	</header>
     	
-    	<section class="container">
+    <!-- MODUL 1 -->
+    	
+    	<section class="container cream">
     		<div class="row">
 	    		<div class="col-md-6 col-xs-12">
 	    			<div class="row">
 	    				<div class="col-md-12">
-	    					<h2></h2>
+	    					<h2>MODULE 1</h2>
 	    				</div>
 	    				<div class="col-md-12">
-	    					<video src=""></video>
+	    					<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
 	    				</div>
 	    			</div>
 				</div>
 				<div class="col-md-6 col-xs-12">
-					<h2></h2>
-					<p></p>
-					<p></p>
+					<h3>What is <span>D</span>ART<span>?</span></h3>
+					<p>This short video (1mn) will introduce you to <span>D</span>ART and how it can help you find the documents you are looking for.</p>
+					<p><span>D</span>ART stands for « Document Archive Retrieval Tool » and was built in 2006 by the team with the same name. It is an extraordinary resource for all the BCS Regulatory community.</p>
 				</div>
-				<div class="col-md-12"></div>
+			</div>
+			<div class="row">	
+				<div class="col-md-12">
+					<p>Did this video answer your questions?<i class="fa fa-check"></i></p>
+				</div>
 			</div>
     	</section>
+    	
+    <!-- MODUL 2 -->	
     		
-    	<section class="container">
+    	<section class="container grey">
     		<div class="row">
     			<div class="col-md-12">
-    				<h2></h2>
+    				<h2>MODULE 2</h2>
     			</div>
     			<div class="col-md-12">
-    				<h3></h3>
-    				<p></p>
+    				<h3>How does <span>D</span>ART work<span>?</span></h3>
+    				<p>You are now convinced that <span>D</span>ART can be a very precious partner in your daily activities. Want to see what it is like and how easy it is to start working with it? The following demos will show you 7 practical use cases (1mn30 each).</p>
     			</div>
     			
     			<article id="active" class="col-md-12">
-    					<video src=""></video>
+    					<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
     			</article>	
     			
     			<article class="col-md-4 col-sm-6 col-xs-12">
     				<div class="row">
     					<div class="col-md-12">
-    						<video src=""></video>
+    						<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
     					</div>
     					<div class="col-md-12">
-    						<h4></h4>
-    						<h5></h5>
+    						<h4>PART 2</h4>
+    						<h5>Multiple search</h5>
     					</div>
     				</div>
     			</article>
@@ -87,11 +111,16 @@
     			<article class="col-md-4 col-sm-6 col-xs-12">
     				<div class="row">
     					<div class="col-md-12">
-    						<video src=""></video>
+    						<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
     					</div>
     					<div class="col-md-12">
-    						<h4></h4>
-    						<h5></h5>
+    						<h4>PART 3</h4>
+    						<h5>Primary document</h5>
     					</div>
     				</div>
     			</article>
@@ -99,11 +128,16 @@
     			<article class="col-md-4 col-sm-6 col-xs-12">
     				<div class="row">
     					<div class="col-md-12">
-    						<video src=""></video>
+    						<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
     					</div>
     					<div class="col-md-12">
-    						<h4></h4>
-    						<h5></h5>
+    						<h4>PART 4</h4>
+    						<h5>Multiple search</h5>
     					</div>
     				</div>
     			</article>
@@ -111,11 +145,16 @@
     			<article class="col-md-4 col-sm-6 col-xs-12">
     				<div class="row">
     					<div class="col-md-12">
-    						<video src=""></video>
+    						<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
     					</div>
     					<div class="col-md-12">
-    						<h4></h4>
-    						<h5></h5>
+    						<h4>PART 5</h4>
+    						<h5>Primary document</h5>
     					</div>
     				</div>
     			</article>
@@ -123,11 +162,16 @@
     			<article class="col-md-4 col-sm-6 col-xs-12">
     				<div class="row">
     					<div class="col-md-12">
-    						<video src=""></video>
+    						<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
     					</div>
     					<div class="col-md-12">
-    						<h4></h4>
-    						<h5></h5>
+    						<h4>PART 6</h4>
+    						<h5>Multiple search</h5>
     					</div>
     				</div>
     			</article>
@@ -135,29 +179,37 @@
     			<article class="col-md-4 col-sm-6 col-xs-12">
     				<div class="row">
     					<div class="col-md-12">
-    						<video src=""></video>
+    						<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
     					</div>
     					<div class="col-md-12">
-    						<h4></h4>
-    						<h5></h5>
+    						<h4>PART 7</h4>
+    						<h5>Primary document</h5>
     					</div>
     				</div>
     			</article>
-    			
-    			<!--  <div class="col-md-12">
-    				<p></p>
-    			</div>	-->
+    		</div>
+    		<div class="row">	
+    			<div class="col-md-12">
+					<p>Did you find these videos useful?<i class="fa fa-check"></i></p>
+				</div>
     		</div>
     	</section>
     	
-    	<section class="container">
+    <!-- MODUL 3 -->	
+    	
+    	<section class="container cream">
     		<div class="row">
     			<div class="col-md-12">
-    				<h2></h2>
+    				<h2>MODULE 3</h2>
     			</div>
     			<div class="col-md-12">
-    				<h3></h3>
-    				<p></p>
+    				<h3>Getting to know <span>D</span>ART better</h3>
+    				<p>Before going further and getting your <span>D</span>ART access rights, there are a few concepts we want you to know.The main DART concepts are illustrated in these 3 short videos (1mn30 each).</p>
     			</div>
     		</div>
     		<div class="row">
@@ -166,11 +218,17 @@
     			<article class="col-md-8 col-sm-12 ">
     				<div class="row">
     					<div class="col-md-12">
-    						<video  width="100%"  src="http://www.sample-videos.com/video/flv/720/big_buck_bunny_720p_1mb.flv" controls></video>
+    						<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
     					</div>
     					<div class="col-md-12">
-    						<h4></h4>
-    						<h5></h5>
+    						<h4>PART 1</h4>
+    						<h5>Primary document and document workflow</h5>
+    						<p>This video comes accross the concept of document workflow. In DART, the document workflow is a key functionality to the system.</p>
     					</div>
     				</div>
     			</article>
@@ -180,11 +238,16 @@
 		    			<article class="col-md-12 col-sm-6 col-xs-12">
 		    				<div class="row">
 		    					<div class="col-md-12">
-		    						<video src=""></video>
+		    						<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
 		    					</div>
 		    					<div class="col-md-12">
-		    						<h4></h4>
-		    						<h5></h5>
+		    						<h4>PART 2</h4>
+		    						<h5>Multiple search</h5>
 		    					</div>
 		    				</div>
 		    			</article>
@@ -192,11 +255,16 @@
 		    			<article class="col-md-12 col-sm-6 col-xs-12">
 		    				<div class="row">
 		    					<div class="col-md-12">
-		    						<video src=""></video>
+		    						<video width="100%" controls="controls">
+								<source src="http://techslides.com/demos/sample-videos/small.webm" type="video/webm">
+							  	<source src="http://techslides.com/demos/sample-videos/small.ogv" type="video/ogg">
+							  	<source src="http://techslides.com/demos/sample-videos/small.mp4" type="video/mp4">
+							  	<source src="http://techslides.com/demos/sample-videos/small.3gp" type="video/3gp">
+							</video>
 		    					</div>
 		    					<div class="col-md-12">
-		    						<h4></h4>
-		    						<h5></h5>
+		    						<h4>PART 3</h4>
+		    						<h5>Multiple search</h5>
 		    					</div>
 		    				</div>
 		    			</article>
@@ -207,24 +275,31 @@
     		</div>
     	</section>
     	
-    	<section class="container">
+    <!-- RED ZONE -->	
+    	
+    	<section class="container red">
     		<div class="row">
     			<div class="col-md-12">
-    				<p></p>
-    				<p></p>
+    				<p>Almost done!</p>
+    				<p>Now that you a pretty goog idea of what <span>D</span>ART is and what can it do for you, let's do a quick roundup:</p>
     			</div>
-    		
-	    		<div class="col-md-12">
-	    			<h2></h2>
-	    		</div>
-	    		<div class="col-md-12">
-	    			<h3></h3>
-	    		</div>
     		</div>
-    		
+    	</section>	
+    	
+    	<!-- FORM ZONE -->		
+    	
+    	<section class="container cream">	
     		<div class="row">
+	    		<div class="col-md-12">
+	    			<h2>FINAL ROUND</h2>
+	    		</div>
+	    		<div class="col-md-12">
+	    			<h3><span>D</span>ART roundup</h3>
+	    		</div>
+    	
 	    		<form action="">
 	    			<div class="col-md-4 col-sm-6 col-xs-12">
+	    				<p>What is a primary document?</p>
 		    			<div class="radio">
 						  	<label>
 						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
@@ -246,6 +321,7 @@
 					</div>
 					
 					<div class="col-md-4 col-sm-6 col-xs-12">
+						<p>What is a primary document?</p>
 		    			<div class="radio">
 						  	<label>
 						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
@@ -267,6 +343,7 @@
 					</div>
 					
 					<div class="col-md-4 col-sm-6 col-xs-12">
+						<p>What is a primary document?</p>
 		    			<div class="radio">
 						  	<label>
 						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
@@ -288,6 +365,7 @@
 					</div>
 					
 					<div class="col-md-4 col-sm-6 col-xs-12">
+						<p>What is a primary document?</p>
 		    			<div class="radio">
 						  	<label>
 						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
@@ -309,6 +387,7 @@
 					</div>
 					
 					<div class="col-md-4 col-sm-6 col-xs-12">
+						<p>What is a primary document?</p>
 		    			<div class="radio">
 						  	<label>
 						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
@@ -330,6 +409,7 @@
 					</div>
 					
 					<div class="col-md-4 col-sm-6 col-xs-12">
+						<p>What is a primary document?</p>
 		    			<div class="radio">
 						  	<label>
 						  		<input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>
